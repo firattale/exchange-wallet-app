91c21979ba190b1930605fd0010707d2f716e8fd
Fırat Tale <talefirat@gmail.com>
2020-09-02 15:06:25 +0300

    Create Wallet Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header/Header.react'
+import Wallet from '../Wallet/Wallet.react'
 import { useDispatch } from 'react-redux';
 import { changeFirstCurrency } from '../../app/exchangeSlice';
 
@@ -8,14 +9,16 @@ const App = () => {
   const dispatch = useDispatch();
   const handleClick = () => dispatch(changeFirstCurrency({ first: "EUR", firstSign: "€" }));
   const handleClick2 = () => dispatch(changeFirstCurrency({ first: "USD", firstSign: "$" }));
-  const handleClick3= () => dispatch(changeFirstCurrency({ first: "GBP", firstSign: "£" }));
-  
+  const handleClick3 = () => dispatch(changeFirstCurrency({ first: "GBP", firstSign: "£" }));
+
   return (
     <div className="App">
       <Header />
-      <button onClick={handleClick}>change EUR</button>
+      <Wallet />
+      <Wallet />
+      {/* <button onClick={handleClick}>change EUR</button>
       <button onClick={handleClick2}>change USD</button>
-      <button onClick={handleClick3}>change GBP</button>
+      <button onClick={handleClick3}>change GBP</button> */}
     </div>
   );
 }
---
+++ src/components/Wallet/Wallet.react.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Wallet.css"
+
+const Wallet = () => {
+    return <div className="wallet-container">Wallet</div>
+}
+
+
+export default Wallet;
