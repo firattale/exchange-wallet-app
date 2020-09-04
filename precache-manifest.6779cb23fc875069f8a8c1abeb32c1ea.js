b841bbd33c2a3a9261d7a0d4b55703ab0de3bbf9
Fırat Tale <talefirat@gmail.com>
2020-09-04 14:53:50 +0300

    Updates

---
+++ precache-manifest.6779cb23fc875069f8a8c1abeb32c1ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40c3765af5b5f46313c02db0878f84eb",
+    "url": "/revolut-app/index.html"
+  },
+  {
+    "revision": "d2a13a08821493f813e4",
+    "url": "/revolut-app/static/css/main.b73309c9.chunk.css"
+  },
+  {
+    "revision": "9f94648d8398ad729fa7",
+    "url": "/revolut-app/static/js/2.343d6aed.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/revolut-app/static/js/2.343d6aed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a13a08821493f813e4",
+    "url": "/revolut-app/static/js/main.c91ebcc1.chunk.js"
+  },
+  {
+    "revision": "cee282c2d2a3363bd9df",
+    "url": "/revolut-app/static/js/runtime-main.8d99ff33.js"
+  }
+]);
